c4bffe795a859238f89630d555ac8e231e9f6c5b
Hiroaki Nishi <west@keio.jp>
2022-06-03 12:13:04 +0900

    separate codes to test and main

--- STATE/state.v
+++ STATE/state.v
@@ -22,26 +22,3 @@ module state(a, b, s, clk, rst);
 			endcase
 	end
 endmodule
-module test;
-	logic a, b;
-	logic [1:0] s;
-	logic clk, rst;
-	state state(a, b, s, clk, rst);
-	always #5 clk = ~clk;
-initial begin
-	$dumpfile("state.vcd");
-	$dumpvars(0, test);
-clk = 0;
-rst = 1;
-#20
-rst = 0;
-a= 0;
-b= 0;
-#10
-a= 1;
-#50
-b= 1;
-#50
-$finish;
-end
-endmodule
