06f5759ceb6c212c00582198166551e3731834e0
Hiroaki Nishi <west@sd.keio.ac.jp>
2022-06-16 01:52:39 +0900

    change in to wd (write data)

--- TCPU-ex-li/rega.v
+++ TCPU-ex-li/rega.v
@@ -3,11 +3,11 @@ module rega(
 	output logic[15:0] outa, outb,
 	input en,
 	input logic [1:0] wad,
-	input logic [15:0] in,
+	input logic [15:0] wd,
 	input clk, rst);
 	logic [15:0] regar[3:0];
 	always_ff @(posedge clk)
-		if(en) regar[wad] <= in;
+		if(en) regar[wad] <= wd;
 	assign outa = regar[arad];
 	assign outb = regar[brad];
 endmodule
